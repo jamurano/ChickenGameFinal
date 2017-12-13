8d675896710e053ea2d38045901d3f6bb15c7343
Unknown <jamurano@yahoo.com>
2017-12-13 16:33:49 -0700

    .

--- Chicken Game/Assets/Scripts/LevelMaker.cs
+++ Chicken Game/Assets/Scripts/LevelMaker.cs
@@ -10,6 +10,7 @@ public class LevelMaker : MonoBehaviour {
     public Rigidbody chicken;
     public Rigidbody wolf;
     public Text chickenText;
+    public Transform wolfSpawn;
 
     public static int chickensLeft = 0;
 
@@ -19,30 +20,57 @@ public class LevelMaker : MonoBehaviour {
     }
 
 
-    public void CreateLevel(){
+    public void CreateLevel()
+    {
 
         AddChickens();
-        var wolfSpawn = GameObject.Find("Wolf Spawn").transform;
-
-        wolf.transform.position = wolfSpawn.position;
+        RemoveWolves();
+        AddWolves();
     }
 
-    public void ChickenRemoved() {
+    private void RemoveWolves()
+    {
+        var wolves = GameObject.FindGameObjectsWithTag("wolf");
+        foreach (var wolf in wolves)
+        {
+            Destroy(wolf);
+        }
+    }
+
+    private void AddWolves()
+    {
+        for (int i = 0; i < wolfAmount; i++)
+        {
+            Vector3 location = wolfSpawn.position;
+            location.z = location.z + (wolf.transform.localScale.z * i);
+            var w = Instantiate(wolf, location, Quaternion.Euler(Vector3.forward));
+            w.GetComponent<WolfHealth>().spawnPoint = wolfSpawn;
+            w.GetComponent<WolfAI>().spawn = wolfSpawn;
+        }
+    }
+
+    public void ChickenRemoved()
+    {
         chickensLeft -= 1;
         chickenText.text = chickensLeft.ToString();
         Debug.Log(string.Format("Chickens left: {0}", chickensLeft));
 
-        if(chickensLeft <= 0) {
+        if (chickensLeft <= 0)
+        {
             chickenAmount *= 2;
+            wolfAmount++;
+
             CreateLevel();
         }
     }
 
-    public void AddChickens() {
-        for (int i = 0; i < chickenAmount; i++) {
+    public void AddChickens()
+    {
+        for (int i = 0; i < chickenAmount; i++)
+        {
             Vector3 position = GetRandomChickenLocation();
 
-            var c = Instantiate(chicken, position, Random.rotation);
+            var c = Instantiate(chicken, position, UnityEngine.Random.rotation);
 
             var chickenAI = c.GetComponent<ChickenAI>();
             chickenAI.chickenPen = GameObject.Find("Chicken Pen").transform;
@@ -53,18 +81,19 @@ public class LevelMaker : MonoBehaviour {
         chickenText.text = chickensLeft.ToString();
     }
 
-    public Vector3 GetRandomChickenLocation() {
+    public Vector3 GetRandomChickenLocation()
+    {
         Vector3 position = new Vector3();
 
         var spawnPointBeginning = GameObject.Find("Chicken Spawn Begin").transform;
         var spawnPointEnd = GameObject.Find("Chicken Spawn End").transform;
 
-        position.x = Random.Range(spawnPointBeginning.localPosition.x, spawnPointEnd.localPosition.x);
-        position.y = Random.Range(spawnPointBeginning.localPosition.y, spawnPointEnd.localPosition.y);
+        position.x = UnityEngine.Random.Range(spawnPointBeginning.localPosition.x, spawnPointEnd.localPosition.x);
+        position.y = UnityEngine.Random.Range(spawnPointBeginning.localPosition.y, spawnPointEnd.localPosition.y);
         position.z = 0.5f;
 
         return position;
     }
 
-		
-	}
+
+}
