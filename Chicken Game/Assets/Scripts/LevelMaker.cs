9eb843b213e2bc859d52a79ecc80753bd28e6302
Jessica Murano <jamurano@yahoo.com>
2019-03-16 15:01:44 -0600

    Revert "."
    This reverts commit 8d675896710e053ea2d38045901d3f6bb15c7343.

--- Chicken Game/Assets/Scripts/LevelMaker.cs
+++ Chicken Game/Assets/Scripts/LevelMaker.cs
@@ -10,7 +10,6 @@ public class LevelMaker : MonoBehaviour {
     public Rigidbody chicken;
     public Rigidbody wolf;
     public Text chickenText;
-    public Transform wolfSpawn;
 
     public static int chickensLeft = 0;
 
@@ -20,57 +19,30 @@ public class LevelMaker : MonoBehaviour {
     }
 
 
-    public void CreateLevel()
-    {
+    public void CreateLevel(){
 
         AddChickens();
-        RemoveWolves();
-        AddWolves();
+        var wolfSpawn = GameObject.Find("Wolf Spawn").transform;
+
+        wolf.transform.position = wolfSpawn.position;
     }
 
-    private void RemoveWolves()
-    {
-        var wolves = GameObject.FindGameObjectsWithTag("wolf");
-        foreach (var wolf in wolves)
-        {
-            Destroy(wolf);
-        }
-    }
-
-    private void AddWolves()
-    {
-        for (int i = 0; i < wolfAmount; i++)
-        {
-            Vector3 location = wolfSpawn.position;
-            location.z = location.z + (wolf.transform.localScale.z * i);
-            var w = Instantiate(wolf, location, Quaternion.Euler(Vector3.forward));
-            w.GetComponent<WolfHealth>().spawnPoint = wolfSpawn;
-            w.GetComponent<WolfAI>().spawn = wolfSpawn;
-        }
-    }
-
-    public void ChickenRemoved()
-    {
+    public void ChickenRemoved() {
         chickensLeft -= 1;
         chickenText.text = chickensLeft.ToString();
         Debug.Log(string.Format("Chickens left: {0}", chickensLeft));
 
-        if (chickensLeft <= 0)
-        {
+        if(chickensLeft <= 0) {
             chickenAmount *= 2;
-            wolfAmount++;
-
             CreateLevel();
         }
     }
 
-    public void AddChickens()
-    {
-        for (int i = 0; i < chickenAmount; i++)
-        {
+    public void AddChickens() {
+        for (int i = 0; i < chickenAmount; i++) {
             Vector3 position = GetRandomChickenLocation();
 
-            var c = Instantiate(chicken, position, UnityEngine.Random.rotation);
+            var c = Instantiate(chicken, position, Random.rotation);
 
             var chickenAI = c.GetComponent<ChickenAI>();
             chickenAI.chickenPen = GameObject.Find("Chicken Pen").transform;
@@ -81,19 +53,18 @@ public class LevelMaker : MonoBehaviour {
         chickenText.text = chickensLeft.ToString();
     }
 
-    public Vector3 GetRandomChickenLocation()
-    {
+    public Vector3 GetRandomChickenLocation() {
         Vector3 position = new Vector3();
 
         var spawnPointBeginning = GameObject.Find("Chicken Spawn Begin").transform;
         var spawnPointEnd = GameObject.Find("Chicken Spawn End").transform;
 
-        position.x = UnityEngine.Random.Range(spawnPointBeginning.localPosition.x, spawnPointEnd.localPosition.x);
-        position.y = UnityEngine.Random.Range(spawnPointBeginning.localPosition.y, spawnPointEnd.localPosition.y);
+        position.x = Random.Range(spawnPointBeginning.localPosition.x, spawnPointEnd.localPosition.x);
+        position.y = Random.Range(spawnPointBeginning.localPosition.y, spawnPointEnd.localPosition.y);
         position.z = 0.5f;
 
         return position;
     }
 
-
-}
+		
+	}
--- Chicken Game/Assets/Scripts/WolfAI.cs
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WolfAI : MonoBehaviour
-{
-    public float moveSpeed;
-    public int damage;
+public class WolfAI : MonoBehaviour {
+	public float moveSpeed;
+	public int damage;
 
-    public Transform spawn;
+	public GameObject pcHealth;
 
     private WanderScript _wanderScript;
-    private WanderScript WanderScript
-    {
-        get
-        {
-            if (_wanderScript == null)
-            {
+    private WanderScript WanderScript {
+        get {
+            if(_wanderScript == null) {
                 _wanderScript = GetComponent<WanderScript>();
             }
             return _wanderScript;
@@ -24,44 +20,32 @@ public class WolfAI : MonoBehaviour
     }
     private bool pauseTrigger = false;
 
-    private void Update()
-    {
-        if (transform.position.y <= -1)
-        {
-            transform.position = spawn.position;
-            transform.rotation = spawn.rotation;
-        }
-    }
-
-    // Use this for initialization
-    void OnTriggerStay(Collider other)
-    {
+	// Use this for initialization
+	void OnTriggerStay(Collider other){
 
         //Debug.Log(string.Format("Wolf trigger {0} - paused {1}", other.gameObject.name, pauseTrigger));
 
-        if (!pauseTrigger && (other.gameObject.name == "Player" || other.gameObject.name.Contains("Chicken")))
-        {
-
+        if(!pauseTrigger && (other.gameObject.name == "Player" || other.gameObject.name.Contains("Chicken"))){
+            
             WanderScript.enabled = false;
-            //Debug.Log("Player or Chicken has entered wolfs trigger");
+			//Debug.Log("Player or Chicken has entered wolfs trigger");
             transform.LookAt(other.transform);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
             //WanderScript.enabled = true;
-        }
-    }
+		}
+	}
 
     private void OnTriggerExit(Collider other)
     {
-        if (!WanderScript.enabled)
-        {
+        if (!WanderScript.enabled) {
             WanderScript.enabled = true;
         }
     }
 
     void OnCollisionEnter(Collision other)
-    {
-        StartCoroutine(HandleHitRoutine(other));
-    }
+	{
+        StartCoroutine(HandleHitRoutine(other)); 
+	}
 
     IEnumerator HandleHitRoutine(Collision other)
     {
@@ -80,8 +64,7 @@ public class WolfAI : MonoBehaviour
 
         var chicken = hit.gameObject;
 
-        if (chicken != null && chicken.name.Contains("Chicken"))
-        {
+        if(chicken != null && chicken.name.Contains("Chicken")) {
             Debug.Log("Chicken hit");
 
             Object.Destroy(chicken);
@@ -94,8 +77,7 @@ public class WolfAI : MonoBehaviour
             ScoreManager.SubtractPoints(5);
         }
 
-        if (doWait)
-        {
+        if(doWait) {
             Debug.Log("Wolf waiting.");
             WanderScript.enabled = false;
             pauseTrigger = true;
